afe9a16fc83d6640f2bd3bae013aef24cb012ae8
Jinshining <154496001+Jinshining@users.noreply.github.com>
2024-08-17 17:15:25 +0800

    农作物介绍示例
    土豆！！！

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QMessageBox>
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -12,3 +14,9 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_pushButton_clicked()
+{
+    QMessageBox::information(this, tr("营养价值"), tr("土豆具有很高的营养价值和药用价值，其营养素丰富，含有蛋白质、矿物质(磷、钙等)、维生素等多种成分，营养结构也较合理，有“地下苹果”之称。"));
+}
+
